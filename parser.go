1661c24d92a4e7370289a5666fac6a01ae58a524
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-02-06 22:20:26 -0400

    chores(parsers): update char parser docs

--- parser.go
+++ parser.go
@@ -4,26 +4,27 @@ import (
 	"fmt"
 )
 
+// Describes the signature function for parsers.
 type Parser[O any] func(string) (string, O, error)
 
 // Takes a target rune and returns a parser which matches the first rune of the input string against the target.
 //
-// If it matches, returns the rest of the string, the matched rune and a nil error.
-// Else returns empty values for the string and rune, and returns a fullfilled error.
+// If it matches, returns the rest of the string, the stringified rune and a nil error.
+// Else returns empty values for the next string and matched string, and returns a fullfilled error.
 //
 // # Examples
 //
 //	func successExample() {
 //		next, parsed, err := Char('H')("Hello world")
 //		fmt.Println(next)   // "ello world"
-//		fmt.Println(parsed) // 'h'
+//		fmt.Println(parsed) // "h"
 //		fmt.Println(err)    // nil
 //	}
 //
 //	func failExample() {
 //		next, parsed, err := Char('K')("Hello world")
 //		fmt.Println(next)   // ""
-//		fmt.Println(parsed) // 0
+//		fmt.Println(parsed) // ""
 //		fmt.Println(err)    // error
 //	}
 func Char(target rune) Parser[string] {
