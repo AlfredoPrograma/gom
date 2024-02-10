c36607168456304c09ef1678cf1116e10a1e189c
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-02-10 00:55:53 -0400

    feature(parsers): implement strict take while parser

--- predicates.go
+++ predicates.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Predicate func(ch rune) bool
 
 // Takes a predicate function and applies it sequentially over each character of the input string until evaluates to false.
@@ -17,6 +19,31 @@ func FlexTakeWhile(predicate Predicate) Parser[string] {
 			}
 
 			parsed += string(ch)
+		}
+
+		return input[len(parsed):], parsed, nil
+	}
+}
+
+// Takes a predicate function and applies it sequentially over each character of the input string until evaluates to false.
+//
+// Returns the rest of the string, the accumulated characters which complied the predicate as string and a nil error.
+//
+// It is a strict parser. It means at least the first character must comply the predicate. If it doesn't, it will return empty values for next string and parsed string, and returns a fullfilled error.
+func StrictTakeWhile(predicate Predicate) Parser[string] {
+	return func(input string) (string, string, error) {
+		var parsed string
+
+		for _, ch := range input {
+			if !predicate(ch) {
+				break
+			}
+
+			parsed += string(ch)
+		}
+
+		if len(parsed) == 0 {
+			return "", "", fmt.Errorf("at least one character should match with the predicate")
 		}
 
 		return input[len(parsed):], parsed, nil
--- predicates_test.go
+++ predicates_test.go
@@ -25,3 +25,47 @@ func TestFlexTakeWhile(t *testing.T) {
 		t.Errorf("should return the rest of the input")
 	}
 }
+
+func TestStrictTakeWhileSuccess(t *testing.T) {
+	// arrange
+	input := "abcd123"
+	expectedParsed := "abcd"
+	expectedNext := "123"
+
+	// act
+	next, parsed, _ := StrictTakeWhile(func(ch rune) bool {
+		return unicode.IsLetter(ch)
+	})(input)
+
+	// assert
+	if parsed != expectedParsed {
+		t.Errorf("should return the parsed character")
+	}
+
+	if next != expectedNext {
+		t.Errorf("should return the rest of the input")
+	}
+}
+
+func TestStrictTakeWhileFail(t *testing.T) {
+	// arrange
+	input := "abcd123"
+
+	// act
+	next, parsed, err := StrictTakeWhile(func(ch rune) bool {
+		return unicode.IsDigit(ch)
+	})(input)
+
+	// assert
+	if parsed != "" {
+		t.Errorf("should return empty parsed string because it failed")
+	}
+
+	if next != "" {
+		t.Errorf("should return empty rest of the input because it failed")
+	}
+
+	if err == nil {
+		t.Error("should return an error because none character complied predicate on strict parser")
+	}
+}
