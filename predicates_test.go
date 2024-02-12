80b3f26e3a5abe862a4e49a2d31a14c566bed029
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-02-11 22:24:36 -0400

    refactor(tests): refactorize take while and strict take while parser test for table driven approach

--- predicates_test.go
+++ predicates_test.go
@@ -1,73 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"unicode"
 )
 
 func TestTakeWhile(t *testing.T) {
-	// arrange
-	input := "abcd123"
-	expectedParsed := "abcd"
-	expectedNext := "123"
-
-	// act
-	next, parsed, _ := TakeWhile(func(ch rune) bool {
-		return unicode.IsLetter(ch)
-	})(input)
-
-	// assert
-	if parsed != expectedParsed {
-		t.Errorf("should return the parsed character")
+	tests := []ParserTestCase[Predicate, string]{
+		{
+			name:  "successful parse",
+			input: "abcd123",
+			params: func(ch rune) bool {
+				return unicode.IsLetter(ch)
+			},
+			want: ParseResult[string]{
+				next:   "123",
+				parsed: "abcd",
+				err:    nil,
+			},
+		},
+		{
+			name:  "predicate dont match",
+			input: "1234abcd",
+			params: func(ch rune) bool {
+				return unicode.IsLetter(ch)
+			},
+			want: ParseResult[string]{
+				next:   "1234abcd",
+				parsed: "",
+				err:    nil,
+			},
+		},
 	}
 
-	if next != expectedNext {
-		t.Errorf("should return the rest of the input")
-	}
+	ExecParserTestCases[Predicate, string](t, TakeWhile, tests)
 }
 
-func TestStrictTakeWhileSuccess(t *testing.T) {
-	// arrange
-	input := "abcd123"
-	expectedParsed := "abcd"
-	expectedNext := "123"
-
-	// act
-	next, parsed, _ := StrictTakeWhile(func(ch rune) bool {
-		return unicode.IsLetter(ch)
-	})(input)
-
-	// assert
-	if parsed != expectedParsed {
-		t.Errorf("should return the parsed character")
+func TestStrictTakeWhile(t *testing.T) {
+	tests := []ParserTestCase[Predicate, string]{
+		{
+			name:  "successful parse",
+			input: "ABCDefgh",
+			params: func(ch rune) bool {
+				return unicode.IsUpper(ch)
+			},
+			want: ParseResult[string]{
+				next:   "efgh",
+				parsed: "ABCD",
+				err:    nil,
+			},
+		},
+		{
+			name:  "predicate dont match error",
+			input: "ABCDefgh",
+			params: func(ch rune) bool {
+				return unicode.IsLower(ch)
+			},
+			want: ParseResult[string]{
+				next:   "",
+				parsed: "",
+				err:    fmt.Errorf("at least one character should match with the predicate"),
+			},
+		},
 	}
 
-	if next != expectedNext {
-		t.Errorf("should return the rest of the input")
-	}
-}
-
-func TestStrictTakeWhileFail(t *testing.T) {
-	// arrange
-	input := "abcd123"
-
-	// act
-	next, parsed, err := StrictTakeWhile(func(ch rune) bool {
-		return unicode.IsDigit(ch)
-	})(input)
-
-	// assert
-	if parsed != "" {
-		t.Errorf("should return empty parsed string because it failed")
-	}
-
-	if next != "" {
-		t.Errorf("should return empty rest of the input because it failed")
-	}
-
-	if err == nil {
-		t.Error("should return an error because none character complied predicate on strict parser")
-	}
+	ExecParserTestCases[Predicate, string](t, StrictTakeWhile, tests)
 }
 
 func TestTakeTill(t *testing.T) {
