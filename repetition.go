e17a7118cbde9c2741ce091e76dbe3a8017dfdff
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-02-18 00:09:20 -0400

    feature(repetition parsers): implement count parser

--- repetition.go
+++ repetition.go
@@ -35,3 +35,23 @@ func StrictMany[O any](parser Parser[O]) Parser[[]O] {
 		return evalRepetition[O](input, parser, STRICT)
 	}
 }
+
+func Count[O any](parser Parser[O], times uint) Parser[[]O] {
+	return func(input string) (string, []O, error) {
+		accumulated := []O{}
+		next := input
+
+		for i := 0; i < int(times); i++ {
+			n, p, e := parser(next)
+
+			if e != nil {
+				return "", []O{}, fmt.Errorf("cannot execute parser %v times", times)
+			}
+
+			accumulated = append(accumulated, p)
+			next = n
+		}
+
+		return next, accumulated, nil
+	}
+}
--- repetition_test.go
+++ repetition_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 	"unicode"
 )
@@ -62,4 +63,58 @@ func TestStrictMany(t *testing.T) {
 	}
 
 	ExecParserTestCases[Parser[string], []string](t, StrictMany, tests)
+}
+
+func TestCount(t *testing.T) {
+	type CountParserParams[O any] struct {
+		times  uint
+		parser Parser[O]
+	}
+
+	type CountParserTestCase[O any] struct {
+		name   string
+		input  string
+		params CountParserParams[O]
+		want   ParseResult[[]O]
+	}
+
+	tests := []CountParserTestCase[string]{
+		{
+			name:  "successful parse",
+			input: "123456end",
+			params: CountParserParams[string]{
+				parser: Take(2),
+				times:  3,
+			},
+			want: ParseResult[[]string]{
+				next:   "end",
+				parsed: []string{"12", "34", "56"},
+				err:    nil,
+			},
+		},
+		{
+			name:  "parser fail error",
+			input: "notAdot",
+			params: CountParserParams[string]{
+				parser: Char('.'),
+				times:  2,
+			},
+			want: ParseResult[[]string]{
+				next:   "",
+				parsed: []string{},
+				err:    fmt.Errorf("cannot execute parser %v times", 2),
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			next, parsed, err := Count[string](tc.params.parser, tc.params.times)(tc.input)
+			got := ParseResult[[]string]{next, parsed, err}
+
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("%s: expected %+v, but got %+v", tc.name, tc.want, got)
+			}
+		})
+	}
 }
